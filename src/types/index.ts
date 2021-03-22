640f0183dd60e3f60369eaf5ab807896bcaa3d3b
circlesharp <circlesharp@live.com>
2021-03-22 21:34:49 +0800

    初始化 axios & example

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Method =
+  'get' | 'post' | 'delete' | 'put' | 'patch' | 'options' | 'head';
+
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
